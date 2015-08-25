2ef3bc62d2b6bc07020f2e4f28f63eff54629516
jamaps <jeffallen89@gmail.com>
2015-08-25 10:26:26 -0400

    continue building :)

--- mosaic_dateset.py
+++ mosaic_dateset.py
@@ -1,29 +1,29 @@
-# script for building a mosaic dataset, adding rasters, and export files
+# script for building a mosaic dataset and adding rasters
 
 # still under construction
-#   Jeff Allen - August 15, 2015
+# Jeff Allen - August 25, 2015
 
 import arcpy
 
-arcpy.env.workspace = "c:/workspace"
+arcpy.env.workspace = r"PATH_NAME"
+
+mosaic_dataset_name = "______________"
+projection = "___________.prj"
+bands = "3"
 
 #create mosaic
-arcpy.CreateMosaicDataset_management("c:/workspace/CreateMD.gdb",\
-                                    "Mosaic Dataset Name", "Projection.prj",\
-                                    "Number of bands", "8_BIT_UNSIGNED")
+arcpy.CreateMosaicDataset_management("Mosaic.gdb",\
+                                    mosaic_dataset_name, projection,\
+                                    "3", "8_BIT_UNSIGNED")
 
-#build footprints to yes
-how????
+arcpy.SetMosaicDatasetProperties_management(
+    "c:/workspace/mdproperties.gdb/Mosaic Dataset Name", clip_to_footprints="CLIP"
+    )
 
-#add rasters to mosaic
-#need to check parameters
+# add rasters to mosaic
+# need to check parameters
 arcpy.AddRastersToMosaicDataset_management(
      mdname,  rastype, inpath, updatecs, updatebnd, updateovr,
      maxlevel, maxcs, maxdim, spatialref, inputdatafilter,
-     subfolder, duplicate, buildpy, calcstats, 
+     subfolder, duplicate, buildpy, calcstats,
      buildthumb, comments, forcesr)
-     
-#save as layer files
-#need to remove footprints from viewport
-#check absolute or relative
-arcpy.SaveToLayerFile_management("studyquadsLyr", "C:/output/studyquadsLyr.lyr", "ABSOLUTE")
