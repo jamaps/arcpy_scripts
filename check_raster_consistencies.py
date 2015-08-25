85c5551234bb41009844c3933450c07e2e362c2d
Jeff Allen <jamaps@users.noreply.github.com>
2015-08-25 11:53:31 -0400

    fixed an error with confirming an answer

--- check_raster_consistencies.py
+++ check_raster_consistencies.py
@@ -9,22 +9,19 @@
 
 # for server path, use r"\\PATH_NAME"
 
-# script last updated by Jeff Allen on August 14, 2015
-
+# script last updated by Jeff Allen on August 25, 2015
 
 import arcpy
 
-arcpy.env.workspace = PATH HERE
+arcpy.env.workspace = r"\\192-86\DFSRoot\Data\allenj\Desktop\test"
 list_of_rasters = arcpy.ListDatasets()
-
 desc = arcpy.Describe(list_of_rasters[0])
 og_num_bands = desc.bandCount
 og_spatial_ref = (desc.spatialReference).name
-
+bands_answer = "yes"
+ref_answer = "yes"
 print "___"
-
 raster_count = 0
-
 for raster in list_of_rasters:
 	file_path = arcpy.env.workspace + "\\" + raster
 	desc = arcpy.Describe(file_path)
@@ -35,16 +32,16 @@ for raster in list_of_rasters:
 	print "___"
 	raster_count = raster_count + 1
 	if desc.bandCount == og_num_bands:
-		bands_answer = "yes, %d" %og_num_bands
+		bands = "%d" %og_num_bands 
 	else:
 		bands_answer = "no"
 	if spatial_ref.name == og_spatial_ref:
-		ref_answer = "yes, %s" %spatial_ref.name
+		ref = "%s" %spatial_ref.name
 	else:
 		ref_answer = "no"
-	
 print "Number of rasters checked: %d" %raster_count
-print "Do all rasters have the same number of bands? %s" %bands_answer
-print "Do all rasters have the same spatial reference? %s" %ref_answer
+print "Do all rasters have the same number of bands? %s" %bands_answer 
+print "Do all rasters have the same spatial reference? %s" %ref_answer 
+
 
 
