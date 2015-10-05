872bff766c8a407e2a2bf65877e9603dca1a71bb
Jeff Allen <jamaps@users.noreply.github.com>
2015-10-05 12:39:48 -0400

    Update and rename mosaic_dateset.py to bulk_export_feature_class.py

---
+++ bulk_export_feature_class.py
@@ -0,0 +1,12 @@
+import arcpy
+from arcpy import env
+
+env.workspace = r"PATH.gdb"
+output_location = "C:\\Users\\.............\\Output_Folder\\"
+
+FeatureList = arcpy.ListFeatureClasses()
+n = 0
+for Feature in FeatureList:
+	arcpy.FeatureClassToFeatureClass_conversion(Feature, output_location, Feature)
+	n = n + 1
+print n
