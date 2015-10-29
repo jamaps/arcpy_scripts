fca747dc6d2cba0a082beab15585f508d3fbb7ef
Jeff Allen <jamaps@users.noreply.github.com>
2015-10-29 17:12:57 -0400

    Update batch_convert_e00_to_shp.py

--- batch_convert_e00_to_shp.py
+++ batch_convert_e00_to_shp.py
@@ -1,5 +1,5 @@
 import arcpy
-arcpy.env.overwriteOutput = True
+import os
 
 #-----------------------------------
 # converts e00 to shp - run in arcmap python window
@@ -8,23 +8,54 @@ arcpy.env.overwriteOutput = True
 # Workspace for interchange files
 arcpy.env.workspace = ws = r'PATH'
 
-# loop e00 and convert to folders
-for f in arcpy.ListFiles('*.e00'):
-    print f
-    print f.split('.')[0]
-    arcpy.ImportFromE00_conversion(f, ws, f.split('.')[0])
-    print 'Converted %s' %f
 
+os.chdir(ws)
+os.getcwd()
+
+
+# loop e00, convert to coverage, and put into custom folders
+for f in arcpy.ListFiles('*.E00'):
+	print f
+	sp = f.split('.')[0]
+	print sp
+	os.mkdir("%s\coverage_%s" %(ws,sp))
+	arcpy.ImportFromE00_conversion(f, ("%s\coverage_%s" %(ws,sp)), sp)
+	print 'Converted %s' %f
+	
 # add to data frame and rename
 mxd = arcpy.mapping.MapDocument("CURRENT")
 df = arcpy.mapping.ListDataFrames(mxd,"*")[0]
-for dataset in arcpy.ListDatasets():
-    print dataset
-    print ws
-    c = ws + r'\%s' %dataset + r'\polygon'
-    print c
-    newlayer = arcpy.mapping.Layer(c)
-    arcpy.mapping.AddLayer(df, newlayer,"BOTTOM")
+
+for d in arcpy.ListFiles('coverage*'):
+	print "%s\%s" %(ws,d)
+	arcpy.env.workspace = "%s\%s" %(ws,d)
+	for dataset in arcpy.ListDatasets():
+		print dataset
+		print ws
+		c = ws + r'\%s\%s' %(d,dataset) + r'\polygon'
+		print c
+		newlayer = arcpy.mapping.Layer(c)
+		arcpy.mapping.AddLayer(df, newlayer,"BOTTOM")
+
+		layers = arcpy.mapping.ListLayers(mxd)
+
+		for lyr in layers:
+			if lyr.name == "polygon":
+				lyr.name = dataset
+	
+	arcpy.env.workspace = ws
+
+	
+arcpy.RefreshTOC()
+
+
+# convert to shapefiles and put in a new directory
+os.mkdir("all_shp_outputs")
+for l in df:
+    print l
+    arcpy.FeatureClassToFeatureClass_conversion(l, "%s/all_shp_outputs" %ws, "%s.shp" %l)
+    arcpy.mapping.RemoveLayer(df, l)
+
 
     layers = arcpy.mapping.ListLayers(mxd)
 
