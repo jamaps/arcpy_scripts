f17b4d24909f8840f1e1bd8fefd59751c81ff8f7
Jeff Allen <jamaps@users.noreply.github.com>
2015-11-19 13:21:09 -0500

    Create batch_dissolve_and_merge.py

---
+++ batch_dissolve_and_merge.py
@@ -0,0 +1,34 @@
+# script which loops through folder of shps, checks geometry, dissolves each to one feature, and merges the dissolved into one shp
+
+
+import arcpy
+import os
+
+arcpy.env.workspace = r"PATH"
+count = 0
+output_location = r"PATH"
+
+for f in arcpy.ListFiles('*.shp'):
+	print f
+	out_file = output_location + "\cma_" + f
+	print out_file
+	arcpy.RepairGeometry_management ("%s" %f)
+	arcpy.Dissolve_management(f, out_file, "", "", "MULTI_PART", "DISSOLVE_LINES")
+	count = count + 1
+
+print count	
+
+arcpy.env.workspace = output_location
+
+count2 = 0
+merge_list = []
+for d in arcpy.ListFiles('*.shp'):
+	print d
+	merge_list.append(d)
+	count2 = count2 + 1
+
+print merge_list
+print count2 - count
+
+merge_output = r"PATH.shp"
+arcpy.Merge_management(merge_list, merge_output)
