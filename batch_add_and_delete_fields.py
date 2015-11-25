9c0810f9fc09b4a3448898beaf9ba4c3fc0d0e9e
Jeff Allen <jamaps@users.noreply.github.com>
2015-11-25 16:05:49 -0500

    Create batch_add_and_delete_fields.py

---
+++ batch_add_and_delete_fields.py
@@ -0,0 +1,21 @@
+import arcpy
+
+arcpy.env.workspace = ws = r"PATH"
+
+count = 0
+
+for f in arcpy.ListFiles('*.shp'):
+	print f
+	
+	#deleting fields
+	arcpy.DeleteField_management(f, ["AREA","PERIMETER"])
+	
+	#adding fields
+	arcpy.AddField_management(f, "CT_NAME", "TEXT", "", "", 7)
+	arcpy.AddField_management(f, "CTUID", "TEXT", "", "", 11)
+	
+	count = count + 1
+
+print count
+
+# refer to arc documentation for more info
