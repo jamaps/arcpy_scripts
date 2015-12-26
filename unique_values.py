d5fe40dbe25ed8edacf10870dfad17a02967c835
Jeff Allen <jamaps@users.noreply.github.com>
2015-12-25 22:01:43 -0500

    Update unique_values.py

--- unique_values.py
+++ unique_values.py
@@ -1,5 +1,4 @@
-#goes through a folder of shps and prints how many unique values are in a specific field if values in another field are > 0
-
+# prints number of unique CTUIDs in each shp
 
 import arcpy
 
@@ -8,21 +7,16 @@ arcpy.env.workspace = ws = r"PATH"
 print "------------------------------"
 
 for f in arcpy.ListFiles('*.shp'):
-	print f
-	rows = arcpy.SearchCursor(f,"","","CTUID; CT_NAME","CTUID")
-	count = 0
-	for row in rows:
-		if float(row.CT_NAME) > 0:
-			row_x = "0"
-			count = count + 1
-			print "no 0s"
-		else:
-			row_x = row.CTUID
-			print "yes 0s"
-		break
+    	print f
+    	row_x = 0
+    	rows = arcpy.SearchCursor(f,"","","CTUID; CT_NAME","CTUID")
+    	count = 0
 	for row in rows:
 		if row_x != row.CTUID:
-			count = count + 1
+		count = count + 1
 		row_x = row.CTUID
 	print count
 	print "------------------------------"
+
+print "number of shps checked:"
+print total_count
