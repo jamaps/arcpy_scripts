392034e6607a2d0d09947abd28327c11ea62bf83
Jeff Allen <jamaps@users.noreply.github.com>
2016-01-14 22:57:49 -0500

    Update batch_add_and_delete_fields.py

--- batch_add_and_delete_fields.py
+++ batch_add_and_delete_fields.py
@@ -1,23 +1,29 @@
-# ex script for adding/deleting the same fields to multiple shps in a folder
+# short script for adding/deleting the same fields to multiple shps in a folder
+
+# ArcPy delete field documentation = http://pro.arcgis.com/en/pro-app/tool-reference/data-management/delete-field.htm
+# ArcPy add field documentation = http://pro.arcgis.com/en/pro-app/tool-reference/data-management/add-field.htm
 
 import arcpy
 
-arcpy.env.workspace = ws = r"PATH"
+# directory path
+arcpy.env.workspace = r"PATH"
 
 count = 0
 
+# loop through shps in the directory
 for f in arcpy.ListFiles('*.shp'):
+	
 	print f
-	
-	#deleting fields
+
+	#deleting fields "AREA" and "PERIMETER"
 	arcpy.DeleteField_management(f, ["AREA","PERIMETER"])
-	
-	#adding fields
-	arcpy.AddField_management(f, "CT_NAME", "TEXT", "", "", 7)
-	arcpy.AddField_management(f, "CTUID", "TEXT", "", "", 11)
-	
+
+	# adding text fields "NAME" a text field with 5 chars and "NUM" a short int
+	arcpy.AddField_management(f, "NAME", "TEXT", "", "", 5)
+	arcpy.AddField_management(f, "NUM", "SHORT")
+
 	count = count + 1
 
+print "------------------------------"
+print "number of shps checked:"
 print count
-
-# refer to arc documentation for more info
